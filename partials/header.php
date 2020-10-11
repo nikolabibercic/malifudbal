4bfb22837b39ae0ccfd16618a48d7cfa73e29486
nikolabibercic <nikolabibercic@gmail.com>
2020-10-11 22:48:08 +0200

    Ubacio sam google fonts

--- partials/header.php
+++ partials/header.php
@@ -8,6 +8,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
+    <!-- Google font -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
+
+    <style>
+        body{
+            font-family: 'Roboto Condensed', sans-serif;
+        }
+
+        .navbar-brand{
+            font-family: 'Chilanka', cursive;
+        }
+    </style>
+
     <title>Turnir u malom fudbalu</title>
 </head>
 <body>
